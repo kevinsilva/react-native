414ce4c23a37699829bd4f07edc3719a97c2a3c6
kevinsilva <kevinsilva.j@gmail.com>
2024-08-29 15:30:36 +0100

    adds platform specific fonts

--- rate-repository-app/src/theme.js
+++ rate-repository-app/src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from 'react-native';
+
 const theme = {
   colors: {
     white: '#f5f5f5',
@@ -14,7 +16,11 @@ const theme = {
     subheading: 16,
   },
   fonts: {
-    main: 'System',
+    main: Platform.select({
+      ios: 'Arial',
+      android: 'Roboto',
+      default: 'System',
+    }),
   },
   fontWeights: {
     normal: '400',
