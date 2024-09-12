c36e7cbac362d70781083e370a35b0c99586172a
kevinsilva <kevinsilva.j@gmail.com>
2024-09-12 17:33:58 +0100

    adds useAuthStorage and context

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -5,8 +5,11 @@ import Constants from 'expo-constants';
 
 import Main from './src/components/Main';
 import createApolloClient from './src/utils/apolloClient';
+import AuthStorage from './src/utils/authStorage';
+import AuthStorageContext from './src/contexts/authStorageContext';
 
-const apolloClient = createApolloClient();
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
 
 export default function App() {
   console.log('constants', Constants.expoConfig);
@@ -15,7 +18,9 @@ export default function App() {
     <>
       <NativeRouter>
         <ApolloProvider client={apolloClient}>
-          <Main />
+          <AuthStorageContext.Provider value={authStorage}>
+            <Main />
+          </AuthStorageContext.Provider>
         </ApolloProvider>
       </NativeRouter>
       <StatusBar style='auto' />
--- rate-repository-app/src/hooks/useSignIn.js
+++ rate-repository-app/src/hooks/useSignIn.js
@@ -1,10 +1,10 @@
 import { useMutation } from '@apollo/client';
 import { AUTHENTICATE } from '../graphql/mutations';
-import AuthStorage from '../utils/authStorage';
+import useAuthStorage from './useAuthStorage';
 
 const useSignIn = () => {
   const [mutate, result] = useMutation(AUTHENTICATE);
-  const authStorage = new AuthStorage();
+  const authStorage = useAuthStorage();
 
   const signIn = async ({ username, password }) => {
     const { data } = await mutate({
